e7e8dd7cecc26fcb80768e63658e3bb4e791174d
Venkata Sai Sandeep Korrapati <vkorrapati@salesforce.com>
2021-07-02 13:31:23 +0530

    fix: Modified git rep url in release config

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   branches: "master",
-  repositoryUrl: "https://github.com/KVSaiSandeep/GithubActionsPoc1",
+  repositoryUrl: "https://github.com/vsskorrapati/GithubActionsPOC.git",
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
